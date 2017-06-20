ce49f24c042267c3672063e04a928cbf01772bf5
Verta-master <vertyagina@gmail.com>
2017-06-20 15:42:05 +0600

    js-menu plus IE corrections

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var navIntro = document.querySelector('.intro');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -7,8 +8,12 @@ navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    navIntro.classList.remove('intro--closed');
+    navIntro.classList.add('intro--opened');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    navIntro.classList.remove('intro--opened');
+    navIntro.classList.add('intro--closed');
   }
 });
